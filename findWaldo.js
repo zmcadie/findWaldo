b7a50112324f0e4e40371f883eb15486f4bf96d6
Zola <zolamcadie@gmail.com>
2017-06-29 18:13:48 +0000

    print the index of waldo when found

---
+++ findWaldo.js
@@ -0,0 +1,13 @@
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(index) {
+  console.log("Found him at index: " + index + '!');
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
